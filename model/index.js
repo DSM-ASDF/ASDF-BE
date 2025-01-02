8b18d83f5e7b37095ff2f9f9b6836a2a6adf3156
limda00 <dylim7715@gmail.com>
2025-01-02 11:48:54 +0900

    🐛 :: 모델 참조 순서 문제 수정

--- model/index.js
+++ model/index.js
@@ -16,9 +16,6 @@ User.initiate(sequelize);
 const Chat = require("./chat.js");
 Chat.initiate(sequelize);
 
-const Comment = require("./comment.js");
-Comment.initiate(sequelize);
-
 const Message = require("./message.js");
 Message.initiate(sequelize);
 
@@ -31,13 +28,16 @@ Team.initiate(sequelize);
 const Todo = require("./todo.js");
 Todo.initiate(sequelize);
 
+const Comment = require("./comment.js");
+Comment.initiate(sequelize);
+
 db.sequelize = sequelize;
 db.User = User;
 db.Chat = Chat;
-db.Comment = Comment;
 db.Message = Message;
 db.Schedule = Schedule;
 db.Team = Team;
 db.Todo = Todo;
+db.Comment = Comment;
 
 module.exports = db;
